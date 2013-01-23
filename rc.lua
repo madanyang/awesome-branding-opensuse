f1d570055648de1bc2aca6309353af4563e70198
madanyang <toganm@opensuse.org>
2013-01-23 16:45:21 +0100

    Personal theme
    If user has a personal theme in user config directory
    ~/.config/awesome/themes/theme.lua then use the personal theme else
    use default opensuse theme
    
    Signed-off-by: madanyang <toganm@opensuse.org>

--- rc.lua
+++ rc.lua
@@ -65,7 +65,24 @@ end
 
 -- {{{ Variable definitions
 -- Themes define colours, icons, and wallpapers
-beautiful.init("/usr/share/awesome/themes/openSUSE/theme.lua")
+-- Use personal theme if existing else goto default
+do
+    local user_theme, ut
+    user_theme = awful.util.getdir("config") .. "/theme/theme.lua"
+    ut = io.open(user_theme)
+    if ut then
+        io.close(ut)
+        beautiful.init(user_theme)
+    else
+        print("Theme doesn't exist, falling back to opeSUSE")
+        beautiful.init("/usr/share/awesome/themes/openSUSE/theme.lua")
+    end
+end
+
+-- beautiful.init("/usr/share/awesome/themes/openSUSE/theme.lua")
+
+
+
 
 -- This is used later as the default terminal and editor to run.
 terminal = "xterm"
